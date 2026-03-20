979b61041c1cec68b54c8f1c8547f5445cbc8efc
文余两人 <523272086@qq.com>
2026-03-21 00:42:26 +0800

    Update DEV_BASE_URL in config.js to use the production URL for development environment, enhancing consistency across environments.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PROD_BASE_URL = 'https://autorecordarchery.xyz';
-const DEV_BASE_URL = 'http://127.0.0.1:41003';
+const DEV_BASE_URL = 'https://autorecordarchery.xyz';
 // const DEV_BASE_URL = 'http://192.168.3.24:41003'; // 真机模拟环境
 
 function getEnvVersion() {
